1dfa88527278c8687cea273db1126cf90f5e3096
Tade Strehk <git@strehk.eu>
2025-12-17 12:29:13 +0100

    v0.1.3

---
+++ Formula/lazyi18n@0.1.3.rb
@@ -0,0 +1,23 @@
+class Lazyi18n < Formula
+  include Language::Python::Virtualenv
+
+  desc "TUI for managing i18n translation files"
+  homepage "https://github.com/Strehk/lazyi18n"
+  license "MIT"
+
+  # The 'url' and 'sha256' will be updated by 'brew bump-formula-pr' or bots
+  url "https://github.com/Strehk/lazyi18n/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "193a9b3e227aeea2a2688b60af14a8e8ef817f30cd696b6de23c0e60fa70b190"
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_create(libexec, "python3.14")
+    system libexec/"bin/python", "-m", "pip", "install", "."
+    bin.install_symlink libexec/"bin/lazyi18n"
+  end
+
+  test do
+    assert_match "lazyi18n", shell_output("#{bin}/lazyi18n --version")
+  end
+end
