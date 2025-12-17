efe91031f34c826931edb3400320d6b239835818
Tade Strehk <git@strehk.eu>
2025-12-17 13:33:30 +0100

    add nightly formula

---
+++ Formula/lazyi18n-nightly.rb
@@ -0,0 +1,23 @@
+class Lazyi18nNightly < Formula
+  include Language::Python::Virtualenv
+
+  desc "TUI for managing i18n translation files"
+  homepage "https://github.com/Strehk/lazyi18n"
+
+  url "https://github.com/Strehk/lazyi18n/releases/download/nightly/lazyi18n-0.1.1.dev20251217122949.tar.gz"
+  sha256 "356e41037785d3d2af0b0f6a61200c7e86fc3ec386bef8fecb3445b3d4f768fb"
+
+  license "MIT"
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_create(libexec, "python3.14")
+    system libexec/"bin/python", "-m", "pip", "install", "."
+    bin.install_symlink libexec/"bin/lazyi18n-nightly"
+  end
+
+  test do
+    assert_match "lazyi18n-nightly", shell_output("#{bin}/lazyi18n-nightly --version")
+  end
+end
